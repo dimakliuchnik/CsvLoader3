ba5faef50aa01d7e3d07db7c40f7dedfd2a04a7e
dzmitryk <dzmitryk@compatibl.com>
2017-12-06 15:56:17 +0300

    Clean up.

--- CsvLoader3/App_Start/RouteConfig.cs
+++ CsvLoader3/App_Start/RouteConfig.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 using System.Web.Routing;
 
 namespace CsvLoader3
@@ -14,10 +10,10 @@ namespace CsvLoader3
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
-                name: "Default",
-                url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
-            );
+                 name: "Default",
+                 url: "{controller}/{action}/{id}",
+                 defaults: new { controller = "Login", action = "Index", id = UrlParameter.Optional }
+             );
         }
     }
 }
--- CsvLoader3/Controllers/FilesController.cs
+++ CsvLoader3/Controllers/FilesController.cs
@@ -1,12 +1,9 @@
 ﻿using CsvLoader3.Models;
 using LumenWorks.Framework.IO.Csv;
-using System;
 using System.Collections.Generic;
-using System.Data;
 using System.IO;
-using System.Net.Http;
-using System.Web;
 using System.Web.Mvc;
+using System.Web.Routing;
 
 namespace CsvLoader3.Controllers
 {
@@ -20,6 +17,7 @@ namespace CsvLoader3.Controllers
         }
 
         // GET: Files
+        [HttpGet]
         public ActionResult Upload()
         {
             return View();
@@ -29,17 +27,18 @@ namespace CsvLoader3.Controllers
         //[ValidateAntiForgeryToken]
         public ActionResult UploadFile()
         {
+            string baseFileName = "";
             foreach (string file in Request.Files)
             {
-                var FileDataContent = Request.Files[file];
-                if (FileDataContent != null && FileDataContent.ContentLength > 0)
+                var fileDataContent = Request.Files[file];
+                if (fileDataContent != null && fileDataContent.ContentLength > 0)
                 {
                     // take the input stream, and save it to a temp folder using the original file.part name posted
-                    var stream = FileDataContent.InputStream;
-                    var fileName = Path.GetFileName(FileDataContent.FileName);
-                    var UploadPath = Server.MapPath("~/App_Data/uploads");
-                    Directory.CreateDirectory(UploadPath);
-                    string path = Path.Combine(UploadPath, fileName);
+                    var stream = fileDataContent.InputStream;
+                    var fileName = Path.GetFileName(fileDataContent.FileName);
+                    var uploadPath = Server.MapPath("~/App_Data/uploads");
+                    Directory.CreateDirectory(uploadPath);
+                    string path = Path.Combine(uploadPath, fileName);
                     try
                     {
                         if (System.IO.File.Exists(path))
@@ -55,9 +54,9 @@ namespace CsvLoader3.Controllers
                         if (result)
                         {
                             string partToken = ".part_";
-                            string baseFileName = path.Substring(0, path.IndexOf(partToken));
-                            string Searchpattern = Path.GetFileName(baseFileName) + partToken + "*";
-                            string[] filesList = Directory.GetFiles(Path.GetDirectoryName(path), Searchpattern);
+                            baseFileName = path.Substring(0, path.IndexOf(partToken));
+                            string searchPattern = Path.GetFileName(baseFileName) + partToken + "*";
+                            string[] filesList = Directory.GetFiles(Path.GetDirectoryName(path), searchPattern);
                             foreach (var s in filesList)
                             {
                                 try
@@ -67,9 +66,7 @@ namespace CsvLoader3.Controllers
                                 catch (IOException e)
                                 {
                                 }
-
                             }
-                            return RedirectToAction("Upload", "Files");
                         }
                     }
                     catch (IOException ex)
@@ -77,34 +74,35 @@ namespace CsvLoader3.Controllers
                         // handle
                     }
                 }
+
             }
-            return Redirect(Request.UrlReferrer.ToString());
+            var parameters = new RouteValueDictionary { { "file", baseFileName } };
+
+            return RedirectToAction("Upload", "Files", parameters);
+
             //return new HttpResponseMessage()
             //{
             //    StatusCode = System.Net.HttpStatusCode.OK,
             //    Content = new StringContent("File uploaded.")
             //};
-
         }
 
         [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Upload(HttpPostedFileBase file)
+        //[ValidateAntiForgeryToken]
+        public ActionResult Upload(string file)
         {
-            FilesModel filesModel = new FilesModel();
-
             if (ModelState.IsValid)
             {
-                if ( file != null && file.FileName != "")
+                if (file != null && file != "")
                 {
-                    if (file.FileName.EndsWith(".csv"))
+                    if (file.EndsWith(".csv"))
                     {
                         string path = Path.Combine(Server.MapPath("~/Images"),
-                            Path.GetFileName(file.FileName));
+                            Path.GetFileName(file));
                         //file.SaveAs(path);
                         ViewBag.Message = "File uploaded successfully";
 
-                        LoadCsvAndFillLoaderModel(file, filesModel);
+                        FilesModel filesModel = LoadCsvAndFillLoaderModel(file);
                         List<FilesModel> models = _filesRepository.GetAllObjectsList();
 
                         bool alreadyLoaded = false;
@@ -114,7 +112,6 @@ namespace CsvLoader3.Controllers
                             {
                                 alreadyLoaded = true;
                                 break;
-
                             }
                         }
                         if (alreadyLoaded)
@@ -125,7 +122,6 @@ namespace CsvLoader3.Controllers
 
                         _filesRepository.Create(filesModel);
                         models.Add(filesModel);
-                        //List<FilesModel> models = _filesRepository.GetAllObjectsList();
 
                         return View(models);
                     }
@@ -145,22 +141,24 @@ namespace CsvLoader3.Controllers
             return View();
         }
 
-        private static void LoadCsvAndFillLoaderModel(HttpPostedFileBase file, FilesModel filesModel)
+        private static FilesModel LoadCsvAndFillLoaderModel(string file)
         {
-            Stream stream = file.InputStream;
-            using (var csvReader = new CsvReader(new StreamReader(stream), true))
+            FilesModel filesModel = new FilesModel();
+            using (StreamReader sr = new StreamReader(file))
             {
-                var headers = csvReader.GetFieldHeaders();
-                filesModel.Header = "";
-                foreach (var column in headers)
+                using (var csvReader = new CsvReader(sr, true))
                 {
-                    filesModel.Header = string.Concat(filesModel.Header, "\n", column);
+                    var headers = csvReader.GetFieldHeaders();
+                    filesModel.Header = "";
+                    foreach (var column in headers)
+                    {
+                        filesModel.Header = string.Concat(filesModel.Header, "\n", column);
+                    }
+                    filesModel.Header = filesModel.Header.Trim();
+                    filesModel.FileName = file;
                 }
-                filesModel.Header = filesModel.Header.Trim();
-                filesModel.FileName = file.FileName;
             }
+            return filesModel;
         }
     }
-
-   
 }
