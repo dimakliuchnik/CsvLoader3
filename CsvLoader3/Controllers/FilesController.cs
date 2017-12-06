f40e623f59a1e4089005e062613ad407f71e765e
dzmitryk <dzmitryk@compatibl.com>
2017-12-06 14:13:30 +0300

    Try to merge logic.

--- CsvLoader3/Controllers/FilesController.cs
+++ CsvLoader3/Controllers/FilesController.cs
@@ -1,10 +1,9 @@
-﻿using System;
-using CsvLoader3.Models;
+﻿using CsvLoader3.Models;
 using LumenWorks.Framework.IO.Csv;
+using System;
 using System.Collections.Generic;
 using System.Data;
 using System.IO;
-using System.Linq;
 using System.Net.Http;
 using System.Web;
 using System.Web.Mvc;
@@ -26,53 +25,9 @@ namespace CsvLoader3.Controllers
             return View();
         }
 
-
-
-
-        //// generic file post method - use in MVC or WebAPI
-        //[HttpPost]
-        //public HttpResponseMessage Upload()
-        //{
-        //    foreach (string file in Request.Files)
-        //    {
-        //        var FileDataContent = Request.Files[file];
-        //        if (FileDataContent != null && FileDataContent.ContentLength > 0)
-        //        {
-        //            // take the input stream, and save it to a temp folder using the original file.part name posted
-        //            var stream = FileDataContent.InputStream;
-        //            var fileName = Path.GetFileName(FileDataContent.FileName);
-        //            var UploadPath = Server.MapPath("~/App_Data/uploads");
-        //            Directory.CreateDirectory(UploadPath);
-        //            string path = Path.Combine(UploadPath, fileName);
-        //            try
-        //            {
-        //                if (System.IO.File.Exists(path))
-        //                    System.IO.File.Delete(path);
-        //                using (var fileStream = System.IO.File.Create(path))
-        //                {
-        //                    stream.CopyTo(fileStream);
-        //                }
-        //                // Once the file part is saved, see if we have enough to merge it
-        //                Utils UT = new Utils();
-        //                UT.MergeFile(path);
-        //            }
-        //            catch (IOException ex)
-        //            {
-        //                // handle
-        //            }
-        //        }
-        //    }
-        //    return new HttpResponseMessage()
-        //    {
-        //        StatusCode = System.Net.HttpStatusCode.OK,
-        //        Content = new StringContent("File uploaded.")
-        //    };
-        //}
-
-
         [HttpPost]
         //[ValidateAntiForgeryToken]
-        public HttpResponseMessage UploadFile()
+        public ActionResult UploadFile()
         {
             foreach (string file in Request.Files)
             {
@@ -95,7 +50,27 @@ namespace CsvLoader3.Controllers
                         }
                         // Once the file part is saved, see if we have enough to merge it
                         Utils UT = new Utils();
-                        UT.MergeFile(path);
+                        bool result = UT.MergeFile(path);
+
+                        if (result)
+                        {
+                            string partToken = ".part_";
+                            string baseFileName = path.Substring(0, path.IndexOf(partToken));
+                            string Searchpattern = Path.GetFileName(baseFileName) + partToken + "*";
+                            string[] filesList = Directory.GetFiles(Path.GetDirectoryName(path), Searchpattern);
+                            foreach (var s in filesList)
+                            {
+                                try
+                                {
+                                    System.IO.File.Delete(s);
+                                }
+                                catch (IOException e)
+                                {
+                                }
+
+                            }
+                            return RedirectToAction("Upload", "Files");
+                        }
                     }
                     catch (IOException ex)
                     {
@@ -103,62 +78,76 @@ namespace CsvLoader3.Controllers
                     }
                 }
             }
-            return new HttpResponseMessage()
-            {
-                StatusCode = System.Net.HttpStatusCode.OK,
-                Content = new StringContent("File uploaded.")
-            };
-
-
-
-
-
-
-
-
-
-
-
-
-            //FilesModel filesModel = new FilesModel();
-
-            //if (ModelState.IsValid)
+            return Redirect(Request.UrlReferrer.ToString());
+            //return new HttpResponseMessage()
             //{
-            //    if (upload != null && upload.ContentLength > 0)
-            //    {
-            //        if (upload.FileName.EndsWith(".csv"))
-            //        {
-            //            string path = Path.Combine(Server.MapPath("~/Images"),
-            //                Path.GetFileName(upload.FileName));
-            //            upload.SaveAs(path);
-            //            ViewBag.Message = "File uploaded successfully";
+            //    StatusCode = System.Net.HttpStatusCode.OK,
+            //    Content = new StringContent("File uploaded.")
+            //};
 
-            //            LoadCsvAndFillLoaderModel(upload, filesModel);
-            //            _filesRepository.Create(filesModel);
-
-            //            List<FilesModel> models = _filesRepository.GetAllObjectsList();
-
-            //            return View(models);
-            //        }
-            //        else
-            //        {
-            //            ModelState.AddModelError("File", "This file format is not supported");
-            //            return View();
-            //        }
-            //    }
-            //    else
-            //    {
-            //        ModelState.AddModelError("File", "Please Upload Your file");
-            //        List<FilesModel> models = _filesRepository.GetAllObjectsList();
-            //        return View(models);
-            //    }
-            //}
-            //return View();
         }
 
-        private static void LoadCsvAndFillLoaderModel(HttpPostedFileBase upload, FilesModel filesModel)
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Upload(HttpPostedFileBase file)
         {
-            Stream stream = upload.InputStream;
+            FilesModel filesModel = new FilesModel();
+
+            if (ModelState.IsValid)
+            {
+                if ( file != null && file.FileName != "")
+                {
+                    if (file.FileName.EndsWith(".csv"))
+                    {
+                        string path = Path.Combine(Server.MapPath("~/Images"),
+                            Path.GetFileName(file.FileName));
+                        //file.SaveAs(path);
+                        ViewBag.Message = "File uploaded successfully";
+
+                        LoadCsvAndFillLoaderModel(file, filesModel);
+                        List<FilesModel> models = _filesRepository.GetAllObjectsList();
+
+                        bool alreadyLoaded = false;
+                        foreach (var model in models)
+                        {
+                            if (model.FileName == filesModel.FileName && model.Header == filesModel.Header)
+                            {
+                                alreadyLoaded = true;
+                                break;
+
+                            }
+                        }
+                        if (alreadyLoaded)
+                        {
+                            ModelState.AddModelError("File", "This file has been already loaded");
+                            return View(models);
+                        }
+
+                        _filesRepository.Create(filesModel);
+                        models.Add(filesModel);
+                        //List<FilesModel> models = _filesRepository.GetAllObjectsList();
+
+                        return View(models);
+                    }
+                    else
+                    {
+                        ModelState.AddModelError("File", "This file format is not supported");
+                        return View();
+                    }
+                }
+                else
+                {
+                    ModelState.AddModelError("File", "Please choose Your file");
+                    List<FilesModel> models = _filesRepository.GetAllObjectsList();
+                    return View(models);
+                }
+            }
+            return View();
+        }
+
+        private static void LoadCsvAndFillLoaderModel(HttpPostedFileBase file, FilesModel filesModel)
+        {
+            Stream stream = file.InputStream;
             using (var csvReader = new CsvReader(new StreamReader(stream), true))
             {
                 var headers = csvReader.GetFieldHeaders();
@@ -168,146 +157,10 @@ namespace CsvLoader3.Controllers
                     filesModel.Header = string.Concat(filesModel.Header, "\n", column);
                 }
                 filesModel.Header = filesModel.Header.Trim();
-                filesModel.FileName = upload.FileName;
+                filesModel.FileName = file.FileName;
             }
         }
     }
 
-
-
-    class Utils
-    {
-        public string FileName { get; set; }
-        public string TempFolder { get; set; }
-        public int MaxFileSizeMB { get; set; }
-        public List<String> FileParts { get; set; }
-
-        public Utils()
-        {
-            FileParts = new List<string>();
-        }
-
-        /// <summary>
-        /// original name + ".part_N.X" (N = file part number, X = total files)
-        /// Objective = enumerate files in folder, look for all matching parts of split file. If found, merge and return true.
-        /// </summary>
-        /// <param name="FileName"></param>
-        /// <returns></returns>
-        public bool MergeFile(string FileName)
-        {
-            bool rslt = false;
-            // parse out the different tokens from the filename according to the convention
-            string partToken = ".part_";
-            string baseFileName = FileName.Substring(0, FileName.IndexOf(partToken));
-            string trailingTokens = FileName.Substring(FileName.IndexOf(partToken) + partToken.Length);
-            int FileIndex = 0;
-            int FileCount = 0;
-            int.TryParse(trailingTokens.Substring(0, trailingTokens.IndexOf(".")), out FileIndex);
-            int.TryParse(trailingTokens.Substring(trailingTokens.IndexOf(".") + 1), out FileCount);
-            // get a list of all file parts in the temp folder
-            string Searchpattern = Path.GetFileName(baseFileName) + partToken + "*";
-            string[] FilesList = Directory.GetFiles(Path.GetDirectoryName(FileName), Searchpattern);
-            //  merge .. improvement would be to confirm individual parts are there / correctly in sequence, a security check would also be important
-            // only proceed if we have received all the file chunks
-            if (FilesList.Count() == FileCount)
-            {
-                // use a singleton to stop overlapping processes
-                if (!MergeFileManager.Instance.InUse(baseFileName))
-                {
-                    MergeFileManager.Instance.AddFile(baseFileName);
-                    if (File.Exists(baseFileName))
-                        File.Delete(baseFileName);
-                    // add each file located to a list so we can get them into 
-                    // the correct order for rebuilding the file
-                    List<SortedFile> MergeList = new List<SortedFile>();
-                    foreach (string File in FilesList)
-                    {
-                        SortedFile sFile = new SortedFile();
-                        sFile.FileName = File;
-                        baseFileName = File.Substring(0, File.IndexOf(partToken));
-                        trailingTokens = File.Substring(File.IndexOf(partToken) + partToken.Length);
-                        int.TryParse(trailingTokens.Substring(0, trailingTokens.IndexOf(".")), out FileIndex);
-                        sFile.FileOrder = FileIndex;
-                        MergeList.Add(sFile);
-                    }
-                    // sort by the file-part number to ensure we merge back in the correct order
-                    var MergeOrder = MergeList.OrderBy(s => s.FileOrder).ToList();
-                    using (FileStream FS = new FileStream(baseFileName, FileMode.Create))
-                    {
-                        // merge each file chunk back into one contiguous file stream
-                        foreach (var chunk in MergeOrder)
-                        {
-                            try
-                            {
-                                using (FileStream fileChunk = new FileStream(chunk.FileName, FileMode.Open))
-                                {
-                                    fileChunk.CopyTo(FS);
-                                }
-                            }
-                            catch (IOException ex)
-                            {
-                                // handle                                
-                            }
-                        }
-                    }
-                    foreach (var chunk in MergeOrder)
-                    {
-                       // System.IO.File.Delete(chunk.FileName);
-                    }
-                    rslt = true;
-                    // unlock the file from singleton
-                    MergeFileManager.Instance.RemoveFile(baseFileName);
-                }
-            }
-            return rslt;
-        }
-
-
-    }
-
-    public struct SortedFile
-    {
-        public int FileOrder { get; set; }
-        public String FileName { get; set; }
-    }
-
-    public class MergeFileManager
-    {
-        private static MergeFileManager instance;
-        private List<string> MergeFileList;
-
-        private MergeFileManager()
-        {
-            try
-            {
-                MergeFileList = new List<string>();
-            }
-            catch { }
-        }
-
-        public static MergeFileManager Instance
-        {
-            get
-            {
-                if (instance == null)
-                    instance = new MergeFileManager();
-                return instance;
-            }
-        }
-
-        public void AddFile(string BaseFileName)
-        {
-            MergeFileList.Add(BaseFileName);
-        }
-
-        public bool InUse(string BaseFileName)
-        {
-            return MergeFileList.Contains(BaseFileName);
-        }
-
-        public bool RemoveFile(string BaseFileName)
-        {
-            return MergeFileList.Remove(BaseFileName);
-        }
-    }
+   
 }
---
+++ CsvLoader3/Controllers/Utils.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace CsvLoader3.Controllers
+{
+    public class Utils
+    {
+        public string FileName { get; set; }
+        public string TempFolder { get; set; }
+        public int MaxFileSizeMB { get; set; }
+        public List<string> FileParts { get; set; }
+
+        public Utils()
+        {
+            FileParts = new List<string>();
+        }
+
+        /// <summary>
+        /// original name + ".part_N.X" (N = file part number, X = total files)
+        /// Objective = enumerate files in folder, look for all matching parts of split file. If found, merge and return true.
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public bool MergeFile(string fileName)
+        {
+            bool rslt = false;
+            // parse out the different tokens from the filename according to the convention
+            string partToken = ".part_";
+            string baseFileName = fileName.Substring(0, fileName.IndexOf(partToken));
+            string trailingTokens = fileName.Substring(fileName.IndexOf(partToken) + partToken.Length);
+            int fileIndex = 0;
+            int fileCount = 0;
+            int.TryParse(trailingTokens.Substring(0, trailingTokens.IndexOf(".")), out fileIndex);
+            int.TryParse(trailingTokens.Substring(trailingTokens.IndexOf(".") + 1), out fileCount);
+            // get a list of all file parts in the temp folder
+            string Searchpattern = Path.GetFileName(baseFileName) + partToken + "*";
+            string[] filesList = Directory.GetFiles(Path.GetDirectoryName(fileName), Searchpattern);
+            //  merge .. improvement would be to confirm individual parts are there / correctly in sequence, a security check would also be important
+            // only proceed if we have received all the file chunks
+            if (filesList.Count() == fileCount)
+            {
+                // use a singleton to stop overlapping processes
+                if (!MergeFileManager.Instance.InUse(baseFileName))
+                {
+                    MergeFileManager.Instance.AddFile(baseFileName);
+                    if (File.Exists(baseFileName))
+                        File.Delete(baseFileName);
+                    // add each file located to a list so we can get them into
+                    // the correct order for rebuilding the file
+                    List<SortedFile> mergeList = new List<SortedFile>();
+                    foreach (string file in filesList)
+                    {
+                        SortedFile sFile = new SortedFile();
+                        sFile.FileName = file;
+                        baseFileName = file.Substring(0, file.IndexOf(partToken));
+                        trailingTokens = file.Substring(file.IndexOf(partToken) + partToken.Length);
+                        int.TryParse(trailingTokens.Substring(0, trailingTokens.IndexOf(".")), out fileIndex);
+                        sFile.FileOrder = fileIndex;
+                        mergeList.Add(sFile);
+                    }
+                    // sort by the file-part number to ensure we merge back in the correct order
+                    var mergeOrder = mergeList.OrderBy(s => s.FileOrder).ToList();
+                    using (FileStream FS = new FileStream(baseFileName, FileMode.Create))
+                    {
+                        // merge each file chunk back into one contiguous file stream
+                        foreach (var chunk in mergeOrder)
+                        {
+                            try
+                            {
+                                using (FileStream fileChunk = new FileStream(chunk.FileName, FileMode.Open))
+                                {
+                                    fileChunk.CopyTo(FS);
+                                    System.IO.File.Delete(chunk.FileName);
+                                }
+                            }
+                            catch (IOException ex)
+                            {
+                                // handle
+                            }
+                        }
+                    }
+                    rslt = true;
+                    // unlock the file from singleton
+                    MergeFileManager.Instance.RemoveFile(baseFileName);
+                }
+            }
+            return rslt;
+        }
+
+        protected virtual bool IsFileLocked(FileInfo file)
+        {
+            FileStream stream = null;
+
+            try
+            {
+                stream = file.Open(FileMode.Open, FileAccess.ReadWrite, FileShare.None);
+            }
+            catch (IOException)
+            {
+                //the file is unavailable because it is:
+                //still being written to
+                //or being processed by another thread
+                //or does not exist (has already been processed)
+                return true;
+            }
+            finally
+            {
+                if (stream != null)
+                    stream.Close();
+            }
+
+            //file is not locked
+            return false;
+        }
+    }
+
+   
+
+
+
+
+    public struct SortedFile
+    {
+        public int FileOrder { get; set; }
+        public string FileName { get; set; }
+    }
+
+    public class MergeFileManager
+    {
+        private static MergeFileManager instance;
+        private List<string> MergeFileList;
+
+        private MergeFileManager()
+        {
+            try
+            {
+                MergeFileList = new List<string>();
+            }
+            catch { }
+        }
+
+        public static MergeFileManager Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new MergeFileManager();
+                return instance;
+            }
+        }
+
+        public void AddFile(string BaseFileName)
+        {
+            MergeFileList.Add(BaseFileName);
+        }
+
+        public bool InUse(string BaseFileName)
+        {
+            return MergeFileList.Contains(BaseFileName);
+        }
+
+        public bool RemoveFile(string BaseFileName)
+        {
+            return MergeFileList.Remove(BaseFileName);
+        }
+    }
+}
